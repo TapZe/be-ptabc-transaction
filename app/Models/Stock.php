5ef565066ed9fe49c782ab80142f9b89093d8345
TapZe <nabilmuyassar@Gmail.com>
2024-11-06 21:05:43 +0800

    feat: added product stock and migration

---
+++ app/Models/Stock.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+
+class Stock extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['stock', 'product_id'];
+
+    /**
+     * Get the product associated with the stock
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function product(): HasOne
+    {
+        return $this->hasOne(Product::class, 'product_id');
+    }
+}
