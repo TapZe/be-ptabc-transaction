ebf7933f3cdb54156b1164514cce0365f5c2d201
TapZe <nabilmuyassar@Gmail.com>
2024-11-06 21:53:19 +0800

    feat: setting up the routes

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,15 @@
 <?php
 
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\TransactionController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware(['authGroup'])->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+
+    Route::resource('transaction', TransactionController::class);
+    Route::resource('product', ProductController::class);
 });
