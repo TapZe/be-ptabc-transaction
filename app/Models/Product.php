880cc174f34a167c5cd4b79d562fb2b356dd3832
TapZe <nabilmuyassar@Gmail.com>
2024-11-06 21:05:08 +0800

    feat: products model and migration

---
+++ app/Models/Product.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+
+class Product extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['name', 'product_type_id'];
+
+    /**
+     * Get the type associated with the Product
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function type(): HasOne
+    {
+        return $this->hasOne(ProductType::class, 'product_type_id');
+    }
+
+    /**
+     * Get the stock associated with the Product
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function stock(): HasOne
+    {
+        return $this->hasOne(Stock::class, 'foreign_key', 'local_key');
+    }
+
+    /**
+     * Get all of the transactions for the Product
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function transactions(): HasMany
+    {
+        return $this->hasMany(Transaction::class);
+    }
+}
