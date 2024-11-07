4170738212a6d0e63a7c249ea50884d7e2b031b6
TapZe <nabilmuyassar@Gmail.com>
2024-11-07 10:14:16 +0800

    fix: comment delete

--- routes/api.php
+++ routes/api.php
@@ -16,9 +16,3 @@ Route::get('/transaction/search', [TransactionController::class, 'searchWithSort
 Route::get('/transaction/typeBought', [TransactionController::class, 'productTypeBoughtList']);
 Route::resource('transaction', TransactionController::class)->except(['create', 'edit']);
 Route::resource('product', ProductController::class);
-
-// Index: GET /transaction - Retrieve a list of transactions.
-// Show: GET /transaction/{id} - Retrieve a specific transaction by ID.
-// Store: POST /transaction - Create a new transaction.
-// Update: PUT /transaction/{id} - Update a specific transaction by ID.
-// Destroy: DELETE /transaction/{id} - Delete a specific transaction by ID.
